7099d7082dd46833c6336317bdc02e983d1aa18d
Mina Wageh <mina.wageh.it@gmail.com>
2025-05-04 04:28:24 +0300

    updates

--- src/SlugServiceProvider.php
+++ src/SlugServiceProvider.php
@@ -8,6 +8,8 @@ class SlugServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
         $this->loadMigrationsFrom(__DIR__ . '/../src/Database/migrations');
         $this->loadViewsFrom(__DIR__ . '/../src/views' , 'wdd/slug');
+        $this->publishes([__DIR__.'/../src/Database/migrations' => database_path('migrations')] , 'slug-migrations');
+
     }
 
     public function register()
